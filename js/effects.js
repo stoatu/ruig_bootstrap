0595b6c8181758fac3a2e008812680c93c5c04f8
stoat <15224038595@163.com>
2016-04-05 12:53:04 +0800

    ruig

---
+++ js/effects.js
@@ -0,0 +1,11 @@
+;(function ($){
+	TouchSlide({ slideCell:"#slideBox", titCell:".hd ul", mainCell:".bd ul", autoPage:true, effect:"leftLoop" });
+}(window.jQuery));
+
+;(function ($){
+	TouchSlide({ slideCell:"#detail_slide",prevCell:'.prev',nextCell:'.next' });
+}(window.jQuery));
+
+;(function ($){
+	TouchSlide({ slideCell:"#slidePhotos", titCell:".hd ul", mainCell:".bd ul", autoPage:true, effect:"leftLoop" });
+}(window.jQuery));
